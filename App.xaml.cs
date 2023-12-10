6d13f6c80bde3ef9334ba6d22d7744c055ebfa8d
Tamas Arpad Both <both.tamas.arpad@gmail.com>
2023-12-10 22:21:13 +0200

    Completed Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,27 @@
-﻿namespace Both_TamasArpad_Lab7;
+﻿using System;
+using Both_TamasArpad_Lab7.Data;
+using System.IO;
+
+namespace Both_TamasArpad_Lab7;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    static ShoppingListDatabase database; 
+    public static ShoppingListDatabase Database 
+    { 
+        get 
+        { 
+            if (database == null) 
+            { 
+                database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3")); 
+            } 
+            return database; 
+        } 
+    }
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new AppShell();
+    }
 }
