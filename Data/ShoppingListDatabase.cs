04196e01cab929cddf1da5f0595e1f8a51467fcb
Tamas Arpad Both <both.tamas.arpad@gmail.com>
2023-12-17 16:20:40 +0200

    Partial progress Laborator10

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -18,6 +18,7 @@ namespace Both_TamasArpad_Lab7.Data
             _database.CreateTableAsync<ShopList>().Wait();
             _database.CreateTableAsync<Product>().Wait();
             _database.CreateTableAsync<ListProduct>().Wait();
+            _database.CreateTableAsync<Shop>().Wait();
         }
         public Task<List<ShopList>> GetShopListsAsync()
         {
@@ -88,6 +89,22 @@ namespace Both_TamasArpad_Lab7.Data
         public Task<int> DeleteListProductAsync(ListProduct listProduct)
         {
             return _database.DeleteAsync(listProduct);
+        }
+
+        public Task<List<Shop>> GetShopsAsync() 
+        { 
+            return _database.Table<Shop>().ToListAsync(); 
+        }
+        public Task<int> SaveShopAsync(Shop shop) 
+        { 
+            if (shop.ID != 0) 
+            { 
+                return _database.UpdateAsync(shop); 
+            } 
+            else 
+            { 
+                return _database.InsertAsync(shop); 
+            } 
         }
     }
 }
--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -12,7 +12,11 @@ public partial class ListPage : ContentPage
     async void OnSaveButtonClicked(object sender, EventArgs e) 
     { 
         var slist = (ShopList)BindingContext; 
-        slist.Date = DateTime.UtcNow; 
+        slist.Date = DateTime.UtcNow;
+
+        Shop selectedShop = (ShopPicker.SelectedItem as Shop);
+        slist.ShopID = selectedShop.ID;
+
         await App.Database.SaveShopListAsync(slist); 
         await Navigation.PopAsync(); 
     }
@@ -33,7 +37,12 @@ public partial class ListPage : ContentPage
 
     protected override async void OnAppearing() 
     { 
-        base.OnAppearing(); 
+        base.OnAppearing();
+
+        var items = await App.Database.GetShopsAsync(); 
+        ShopPicker.ItemsSource = (System.Collections.IList)items; 
+        ShopPicker.ItemDisplayBinding = new Binding("ShopDetails");
+
         var shopl = (ShopList)BindingContext; 
         listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID); 
     }
